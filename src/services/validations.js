1aa83161065678f3c5c19ac946c7a568c07fc0a7
ali beztout <ali.beztout@student.vinci.be>
2024-08-29 16:59:01 +0200

    add : test Case

--- src/services/validations.js
+++ src/services/validations.js
@@ -1,4 +1,5 @@
 // TODO: Refactor validation function to follow the defined validations rules
-const isValid = () => Math.random() >= 0.5;
-
-exports.isValid = isValid;
+const isLeapYear {
+  };
+  
+  exports.isLeapYear = isLeapYear;
